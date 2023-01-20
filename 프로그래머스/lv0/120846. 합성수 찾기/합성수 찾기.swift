a4e43dd1db0d4f14ffa02eb77cd2af31cdf97c4b
신나진 <113892130+nasneyland@users.noreply.github.com>
2023-01-20 20:47:47 +0900

    [level 0] Title: 합성수 찾기, Time: 0.11 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.swift
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.swift
@@ -1,11 +1,13 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    var cnt = 0
-    
-    for i in (1...n) {
-        if ((1...n).filter { i % $0 == 0 }.count > 2) {cnt += 1}
-    }
-    
-    return cnt
+    return (1...n).filter { number in
+       var cnt = 0
+        (1...number).forEach {
+            if number % $0 == 0 {
+                cnt += 1
+            }
+        }
+       return cnt > 2
+    }.count
 }
