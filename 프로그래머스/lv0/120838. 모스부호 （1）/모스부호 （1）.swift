a306c841529f6cfb19c7b7c59bfc1ea0248ed0bc
신나진 <113892130+nasneyland@users.noreply.github.com>
2023-01-20 20:09:53 +0900

    [level 0] Title: 모스부호 (1), Time: 0.09 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.swift
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.swift
@@ -1,8 +1,12 @@
 import Foundation
 
 func solution(_ letter:String) -> String {
-    let morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
-    let alphabet = "abcdefghijklmnopqrstuvwxyz"
-    
-    return letter.components(separatedBy: " ").map {morse.firstIndex(of: $0)!}.map{String(alphabet[alphabet.index(alphabet.startIndex, offsetBy: $0)])}.joined()
+    let morses = [
+            ".-": "a", "-...": "b", "-.-.": "c", "-..": "d", ".": "e", "..-.": "f",
+            "--.": "g", "....": "h", "..": "i", ".---": "j", "-.-": "k", ".-..": "l",
+            "--": "m", "-.": "n", "---": "o", ".--.": "p", "--.-": "q", ".-.": "r",
+            "...": "s", "-": "t", "..-": "u", "...-": "v", ".--": "w", "-..-": "x",
+            "-.--": "y", "--..": "z"
+        ]
+    return letter.split(separator: " ").map {morses[String($0),default:""]}.joined()
 }
