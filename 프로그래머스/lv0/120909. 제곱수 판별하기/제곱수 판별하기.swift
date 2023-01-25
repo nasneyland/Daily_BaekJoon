9a34d8ac095e6e9073f2a5543c18568b817dcd7f
신나진 <113892130+nasneyland@users.noreply.github.com>
2023-01-25 19:23:57 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 16 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.swift
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.swift
@@ -1,7 +1,5 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-//    return (2..<n).filter { $0 * $0 == n }.count >= 1 ? 1 : 2
-    
-    return sqrt(Double(n)) == floor(sqrt(Double(n))) ? 1 : 2
+    return floor(sqrt(Double(n))) == sqrt(Double(n)) ? 1 : 2
 }
