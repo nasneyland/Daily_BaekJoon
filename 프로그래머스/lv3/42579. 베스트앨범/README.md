bc6694399ef19f3fba7c8eecd099dc674b2c08ab
신나진 <113892130+nasneyland@users.noreply.github.com>
2023-01-30 19:45:59 +0900

    [level 3] Title: 베스트앨범, Time: 0.29 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/README.md
+++ 프로그래머스/lv3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 16.4 MB, 시간: 0.28 ms
+메모리: 16.6 MB, 시간: 0.29 ms
 
 ### 구분
 
--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.swift
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.swift
@@ -1,31 +1,19 @@
 import Foundation
 
 func solution(_ genres:[String], _ plays:[Int]) -> [Int] {
-    var playsDic: [Int: (String, Int)] = [:]
-    var genresDic: [String: Int] = [:]
+    var dict: [String: [[Int]]] = [:]
     
-    // 장르와 곡 딕셔너리 선언
-    (0...genres.count-1).forEach { 
-        genresDic[genres[$0], default: 0] += plays[$0] 
-        playsDic[$0] = (genres[$0], plays[$0])
+    // 딕셔너리로 같은 장르끼리 묶기
+    genres.enumerated().forEach { (index, genre) in
+         dict[genre, default: []] += [[index, plays[index]]]
     }
     
-    // 장르와 곡 정렬
-    let sortedGenres = genresDic.sorted {$0.1 > $1.1}
-    let sortedPlays = playsDic.sorted {($0.1.1, -$0.0) > ($1.1.1, -$1.0)}
-    
-    var result: [Int] = []
-    for (i, genre) in sortedGenres.enumerated() {
-        for song in sortedPlays {
-            if result.count != (i+1) * 2 {
-                if song.value.0 == genre.key {
-                    result.append(song.key)
-                }
-            } else {
-                break
-            }
-        }
-    }
-    
-    return result
+    // 1. 속한 노래가 많이 재생된 장르 먼저 수록합니다.
+    // 2. 장르 내에서 많이 재생된 노래를 먼저 수록합니다. (같으면 고유번호 낮은 노래 먼저)
+    // 3. 상위 2개 노래만 인덱스를 출력합니다
+    return dict.values.sorted {
+        $0.reduce(0) {$0 + $1[1]} > $1.reduce(0) {$0 + $1[1]}
+    }.flatMap {
+        $0.sorted {(-$0[0],$0[1]) > (-$0[0],$1[1])}
+        .prefix(2).map{$0[0]}}
 }
