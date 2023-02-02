20619ce8bded7be6be601d1a6def8e143f250762
신나진 <113892130+nasneyland@users.noreply.github.com>
2023-02-02 13:43:03 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.13 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.swift
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.swift
@@ -1,18 +1,20 @@
 import Foundation
 
 func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
-    var reported: [String: Int] = [:]
-    var user: [String: [String]] = [:]
 
-    for r in Set(report) {
-        let splited = r.split(separator: " ").map { String($0) }
-        user[splited[0]] = (user[splited[0]] ?? []) + [splited[1]]
-        reported[splited[1]] = (reported[splited[1]] ?? 0) + 1
+    var reportDic = [String: [String]]()
+    var reportedMemberDic = [String: Int]()
+
+    // 신고당한 회원
+    Set(report).forEach {
+        let reported = $0.components(separatedBy: " ")
+        reportDic[reported[0], default: []].append(reported[1])
+        reportedMemberDic[reported[1], default: 0] += 1
     }
 
     return id_list.map { id in
-        return (user[id] ?? []).reduce(0) {
-            $0 + ((reported[$1] ?? 0) >= k ? 1 : 0)
+        return reportDic[id, default: []].reduce(0) {
+            $0 + (reportedMemberDic[$1, default: 0] >= k ? 1 : 0)
         }
     }
 }
