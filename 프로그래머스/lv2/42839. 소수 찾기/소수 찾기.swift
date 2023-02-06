5f394c372dfec3d9169d559e1d21728816e18cca
신나진 <113892130+nasneyland@users.noreply.github.com>
2023-02-06 15:04:35 +0900

    [level 2] Title: 소수 찾기, Time: 1.46 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.swift
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.swift
@@ -2,30 +2,25 @@ import Foundation
 
 func solution(_ numbers:String) -> Int {
     
-    // 순열
-    func permutation(_ array: [String]) -> [String] {
-    if array.count == 0 { return [] }
-
-    let answerArray = (0..<array.count).flatMap { i -> [String] in
-        var removedArray = array
-        let elem = removedArray.remove(at: i)
-        return [elem] + permutation(removedArray).map { elem + $0 }
-    }
-
-    return answerArray
-}
-    
-    // 소수판별
-    func isPrime(_ num: Int) -> Bool {
-        print(num)
-        if(num<4) {
-            return num == 1 || num == 0 ? false : true
+    // 모든 조합 - 순열
+    func permutation(_ array: [String]) -> Set<String> {
+        let permutatedArray = (0..<array.count).flatMap { i -> [String] in
+            var removedArray = array
+            let elem = removedArray.remove(at: i)
+            return [elem] + permutation(removedArray).map { elem + $0}
         }
-        for i in 2...Int(sqrt(Double(num))) {
-            if (num % i == 0) { return false }
+        
+        return Set(permutatedArray)
+    }
+    
+    // 소수 판별
+    func isPrime(_ num: Int) -> Bool {
+        if num < 4 { return num == 2 || num == 3 ? true : false}
+        for i in (2...Int(sqrt(Double(num)))) {
+            if (num % i == 0) { return false } 
         }
         return true
     }
     
-    return Set(permutation(numbers.map {String($0)}).map{Int($0)!}).filter {isPrime($0)}.count
+    return Set(permutation(numbers.map{String($0)}).map{Int($0)!}).filter{isPrime($0)}.count
 }
