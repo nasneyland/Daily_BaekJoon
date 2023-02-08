9275373c6d932d3e4d3ad1ccad74564cefd15e17
신나진 <113892130+nasneyland@users.noreply.github.com>
2023-02-08 20:22:48 +0900

    [level 3] Title: 섬 연결하기, Time: 0.15 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.swift
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.swift
@@ -1,37 +1,37 @@
 import Foundation
 
 func solution(_ n:Int, _ costs:[[Int]]) -> Int {
-  func findParent(_ node: Int) -> Int {
-    if parent[node] != node {
-      return findParent(parent[node])
-    } else {
-      return node
-    }
-  }
-  
-  func union(_ a: Int, _ b: Int) {
-    let a = findParent(a)
-    let b = findParent(b)
     
-    if a < b {
-      parent[b] = a
-    } else {
-      parent[a] = b
-    }
-  }
-  
-  var totalCost = 0
-  var parent = Array(repeating: 0, count: n+1)
-  (1...n).forEach { parent[$0] = $0 }
-  
-  for cost in costs.sorted(by: { $0[2] < $1[2] }) {
-    let (a, b, dist) = (cost[0], cost[1], cost[2])
+    var totalCost = 0
+    var parent = Array(repeating: 0, count: n+1)
+    (1...n).forEach { parent[$0] = $0 }
     
-    if findParent(a) != findParent(b) {
-      union(a, b)
-      totalCost += dist
+    func findParent(_ node: Int) -> Int {
+        if parent[node] != node {
+            return findParent(parent[node])
+        } else {
+            return node
+        }   
     }
-  }
-  
-  return totalCost
+
+    func union(_ a: Int, _ b: Int) {
+        let a = findParent(a)
+        let b = findParent(b)
+
+        if a < b {
+            parent[b] = a
+        } else {
+            parent[a] = b
+        }
+    }
+
+    for cost in costs.sorted(by: { $0[2] < $1[2] }) {
+        let (a, b, dist) = (cost[0], cost[1], cost[2])
+
+        if findParent(a) != findParent(b) {
+            union(a, b)
+            totalCost += dist
+        }
+    }
+    return totalCost
 }
