4b8cba81d77ffa509fcc046b199d9f4f6485257e
신나진 <113892130+nasneyland@users.noreply.github.com>
2023-02-15 20:33:37 +0900

    [level 1] Title: 소수 만들기, Time: 0.06 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.swift
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.swift
@@ -5,22 +5,26 @@ func solution(_ nums:[Int]) -> Int {
     
     //소수판별함수
     func isPrime(_ num: Int) -> Bool {
-        if num < 4 { return num == 2 || num == 3 ? true : false } 
-        
+        if num < 4 { return num == 2 || num == 3 ? true : false }
+
         for n in (2...Int(sqrt(Double(num)))) {
             if num % n == 0 { return false }
         }
         return true
     }
     
-    // 3가지 조합
-    for (iIndex, i) in nums[0...nums.count-3].enumerated() {
-        for (jIndex, j) in nums[iIndex+1...nums.count-2].enumerated() {
-            for k in nums[iIndex+jIndex+2...nums.count-1] {
-                if isPrime(i+j+k) { cnt += 1 }
-            }
+    //조합함수
+    func combi(_ index: Int, _ nowCombi: [Int]) {
+        if nowCombi.count == 3 {
+            if isPrime(nowCombi.reduce(0, +)) { cnt += 1 }
+            return
+        }
+        for i in index..<nums.count {
+            combi(i + 1, nowCombi + [nums[i]])
         }
     }
+    
+    combi(0, [])
     
     return cnt
 }
