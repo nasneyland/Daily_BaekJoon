da1dca04426e8c40b51f41d0e2b16a3dcfc01a1a
신나진 <113892130+nasneyland@users.noreply.github.com>
2023-02-15 20:08:35 +0900

    [level 1] Title: 소수 만들기, Time: 0.07 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.swift
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.swift
@@ -1,20 +1,26 @@
 import Foundation
 
 func solution(_ nums:[Int]) -> Int {
-    var answer = 0
-    for i in 0..<nums.count-2 {
-        for j in i+1..<nums.count-1 {
-            for k in j+1..<nums.count {
-                if isPrime(num: nums[i] + nums[j] + nums[k]) { answer += 1 }
+    var cnt = 0
+    
+    //소수판별함수
+    func isPrime(_ num: Int) -> Bool {
+        if num < 4 { return num == 2 || num == 3 ? true : false } 
+        
+        for n in (2...Int(sqrt(Double(num)))) {
+            if num % n == 0 { return false }
+        }
+        return true
+    }
+    
+    // 3가지 조합
+    for (iIndex, i) in nums[0...nums.count-3].enumerated() {
+        for (jIndex, j) in nums[iIndex+1...nums.count-2].enumerated() {
+            for k in nums[iIndex+jIndex+2...nums.count-1] {
+                if isPrime(i+j+k) { cnt += 1 }
             }
         }
     }
-    return answer
-}
-
-func isPrime(num: Int) -> Bool {
-    for i in 2...Int(sqrt(Double(num))) {
-        if num % i == 0 { return false }
-    }
-    return true
+    
+    return cnt
 }
