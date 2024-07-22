7cbdc2563bc3f881718ae312fde0e101da6ae997
신디 Cindy <113892130+nasneyland@users.noreply.github.com>
2024-07-22 21:15:39 +0900

    [level 3] Title: 베스트앨범, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ Python3/프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,21 @@
+import collections
+
+def solution(genres, plays):
+    ans = []
+    
+    genres_count = collections.defaultdict(int)
+    genres_songs = collections.defaultdict(list)
+    
+    for idx, genres in enumerate(genres):
+        genres_count[genres] += plays[idx]
+        genres_songs[genres].append((idx, plays[idx]))
+    
+    # ✅딕셔너리 키값으로 정렬하는 방법✅
+    for genre in sorted(genres_count, key= lambda x: -genres_count[x]):
+        # print(genre)
+        genres_songs[genre].sort(key= lambda x: (-x[1], x[0]))
+        
+        for genre in genres_songs[genre][:2]:
+            ans.append(genre[0])
+                
+    return ans
